488e26888a9faffa04c099c4a6e969998bca5bda
NathanSparta1 <nathan.ingleton@googlemail.com>
2020-04-14 11:46:00 +0100

    committing solution

---
+++ Copy Directories/Copy Directories/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CopyFunctions;
+using System.IO;
+
+
+namespace Copy_Directories
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // path to file you want to copy
+        public string SourcePath { get; set; }
+
+        // destination path where copied file will end up
+
+        public string DestinationPath { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearBTNClick(object sender, RoutedEventArgs e)
+        {
+            // clear all boxes in program
+            FileNameTB.Text = "";
+            SourceTB.Text = "";
+            DestinationTB.Text = "";
+           
+
+        }
+        // function that handles the copy single file button on UI
+        private void Destination1BTNClick(object sender, RoutedEventArgs e)
+        {
+            string FullPathToFile = System.IO.Path.Combine(SourceTB.Text, FileNameTB.Text);
+
+            FileInfo fi = new FileInfo(FullPathToFile);
+            
+            // forces user to enter a destination to use button
+            if (string.IsNullOrWhiteSpace(DestinationTB.Text))
+            {
+                MessageBox.Show("Please enter a destination");
+            }
+            else if (fi.Exists)
+            {
+                CopyDirectory.SingleFileCopy(FullPathToFile, DestinationTB.Text,FileNameTB.Text);
+                
+                MessageBox.Show($"{FileNameTB.Text} succefully copied to {DestinationTB.Text}");
+            }
+            else
+            {
+                MessageBox.Show("File copy failed, check FileName or source path");
+            }
+        }
+
+        // function that handles the copy folder button on UI
+        private void CopyAllBTNClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(FileNameTB.Text) && DestinationTB.Text != FileNameTB.Text)
+            {
+                DirectoryInfo dir = new DirectoryInfo(FileNameTB.Text);
+
+
+                // forces user to enter a destination to use button
+                if (string.IsNullOrWhiteSpace(DestinationTB.Text))
+                {
+                    MessageBox.Show("Please enter a destination or source");
+                }
+
+
+                else if (dir.Exists)
+                {
+                    CopyDirectory.DirectoryCopy(FileNameTB.Text, DestinationTB.Text, true);
+                    MessageBox.Show($"{FileNameTB.Text} copied to {DestinationTB.Text}");
+                }
+
+
+
+                else
+                {
+                    MessageBox.Show("Folder copy failed, check full folder path");
+                }
+            }
+        }
+
+    }
+}
---
+++ Copy Directories/CopyFunctions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class CopyDirectory
+    {
+        // function to copy a folder to another location
+        public static void DirectoryCopy(string sourceDirName, string destDirName,
+                                      bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            
+            if (!dir.Exists)
+            {
+                
+                /*throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);*/
+            }
+           
+        
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        // function for single file copy
+        public static void SingleFileCopy(string sourceFileName, string destDirName,string fileName)
+        {
+             string destinationFullPath = System.IO.Path.Combine(destDirName,fileName);
+             
+           
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+
+            }
+
+            // if the source file and destination are not the same copy the file
+            if (sourceFileName != destinationFullPath)
+            {
+                File.Copy(sourceFileName, destinationFullPath, true);
+            }
+        }
+    }
+}
